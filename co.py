2dd71ff6458c1d9bbbfd5c16eefb4a8407b8c7ce
None <None>
2016-02-29 20:09:06 -0800

    modified CO script w/ VLAN host and OVSBridge

--- co.py
+++ co.py
@@ -2,13 +2,13 @@
 import json
 
 from mininet.net import Mininet
-from mininet.node import UserSwitch, DefaultController, RemoteController, Host
+from mininet.node import UserSwitch, OVSBridge, DefaultController, RemoteController, Host
 from mininet.topo import Topo
 from mininet.log import  setLogLevel, info, error, warn
 from mininet.cli import CLI
 from mininet.link import OVSIntf
 from mininet.util import quietRun
-
+from mininet.examples.vlanhost import VLANHost
 from domains import SegmentRoutedDomain
 
 class CO(SegmentRoutedDomain):
@@ -35,27 +35,36 @@ class CO(SegmentRoutedDomain):
             leaf = self.addSwitch('leaf%s0%s' % (self.getId(), sw+1),
                                   cls=UserSwitch, dpopts='--no-local-port')
             l_msw.append(self.noteLeaf(leaf))
-            for h in range(f):
-                self.s2gw[leaf] = '10.%s.%s.254' % (self.getId(), sw+1)
-                host = self.addHost('h%s%s%s' % (self.getId(), sw, f+h+1), cls=IpHost,
-                                    ip='10.%s.%s.%s/24' % (self.getId(), sw+1, (f+h+1)),
-                                    gateway=self.s2gw[leaf])
-                self.addLink(host, leaf)
-                l_h.append(host)
-
-        # add extra intra-fabric sanity-test host to fabric gateway switch
-        fgw = m-1
-        host = self.addHost('h%s%s%s' % (self.getId(), fgw, f+f+1), cls=IpHost,
-                            ip='10.%s.%s.%s/24' % (self.getId(), fgw+1, (f+f+1)),
-                            gateway=self.s2gw[l_msw[fgw]])
-        self.addLink(host, l_msw[fgw])
-        l_h.append(host)
-        
+            #uncomment to attach hosts onto leaf 
+            #for h in range(f):
+            #    self.s2gw[leaf] = '10.%s.%s.254' % (self.getId(), sw+1)
+            #    host = self.addHost('h%s%s%s' % (self.getId(), sw, f+h+1), cls=IpHost,
+            #                        ip='10.%s.%s.%s/24' % (self.getId(), sw+1, (f+h+1)),
+            #                        gateway=self.s2gw[leaf])
+            #    self.addLink(host, leaf)
+            #    l_h.append(host)
 
         # interconnect spines and leaves.
         for spine in l_nsw:
             for leaf in l_msw:
                 self.addLink(spine, leaf)
+
+        # add normal mode OVS + host to EE-side leaf
+        ee = 'leaf%s01' % self.getId()
+        ovs = self.addSwitch('ovs%s000' % self.getId(), cls=OVSBridge)
+        self.addLink(ovs, ee)
+        # if standalone VNF host is needed - uncomment next two lines
+        # vnf = self.addHost('h%s004' % self.getId() )
+        # self.addLink(ovs, vnf)
+
+        # add another EE CpQD + host to EE leaf
+        #cpqd = self.addSwitch('ee%s001' % self.getId(),
+        #                      cls=UserSwitch, dpopts='--no-local-port')
+        cpqd = self.addHost('h111', cls=VLANHost )
+        self.addLink(cpqd, ee)
+        # if standalone customer host is needed - uncomment next two lines
+        # cust = self.addHost('h%s005' % self.getId() )
+        # self.addLink(cpqd, cust)
 
     def toCfg(self):
         """ Dump a file in segment routing config file format. """
@@ -92,8 +101,17 @@ class IpHost(Host):
         self.cmd(mtu)
         self.cmd('ip route add default via %s' % self.gateway)
 
+def attachDev(net, sw, dev):
+    switch = net.get(sw)
+    if hasattr(switch, "attach"):
+        switch.attach(dev)
+    else:
+        switch.cmd('ip link set %s netns %s' % (dev, switch.pid))
+    info("Interface %s is attached to switch %s.\n" % (dev, sw))
+
 def setup(argv):
     ctls = sys.argv[1].split(',')
+    ifs = sys.argv[2].split(',') if len(sys.argv) == 2 else []
     co = CO(1)
     for i in range (len(ctls)):
         co.addController('c%s' % i, controller=RemoteController, ip=ctls[i])
@@ -102,8 +120,11 @@ def setup(argv):
     net = Mininet()
     co.build()
     co.injectInto(net)
-    co.dumpCfg('co.json')
+    #co.dumpCfg('co.json')
 
+    # add external ports - hard-codedish
+    for i in ifs:
+        attachDev(net, 'leaf102', i)
     # start everything
     net.build()
     co.start()
@@ -113,8 +134,9 @@ def setup(argv):
 if __name__ == '__main__':
     setLogLevel('info')
     import sys
-    if len(sys.argv) < 2:
-        print ("Usage: sudo -E ./co.py ctl-set\n\n",
-                "Where ctl-set is a comma-separated list of controller IP's")
+    if len(sys.argv) < 1:
+        print ('Usage: sudo -E ./co.py [ctrls] [interfaces]\n\n',
+               '[ctrls] : a comma-separated list of controller IPs\n',
+               '[interfaces] : a comma-separated list of interfaces to the world (optional)')
     else:
         setup(sys.argv)
